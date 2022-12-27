ca516fa8d2090668260a8c4d722f8bba58533ba7
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2022-12-26 21:54:27 -0300

    feat project study auth, login, jwt in nest

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Post,
+  HttpStatus,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { HttpCode } from '@nestjs/common/decorators/http/http-code.decorator';
+import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @IsPublic()
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
+  }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LoginValidationMiddleware } from './middlewares/login-validation-middewae';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local-strategies';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
